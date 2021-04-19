82e633347a59daf38c16a60033079a428c01497f
AkashPatel18 <48157243+AkashPatel18@users.noreply.github.com>
2021-04-19 21:29:56 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAbaKLU4-S_zImzRgLZiCL8yvpoCDwG3u0",
-    authDomain: "insta2-c1959.firebaseapp.com",
-    projectId: "insta2-c1959",
-    storageBucket: "insta2-c1959.appspot.com",
-    messagingSenderId: "803612702424",
-    appId: "1:803612702424:web:61c1c99be42b2ebde189ad"
+    ///add key
   };
 
 
